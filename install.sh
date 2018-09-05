ac3abb5ad817785ea10d7b5320862bfdadde704b
yzbx <youdaoyzbx@163.com>
2018-09-05 15:26:18 +0800

    udpate install

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 wget https://mirrors.ustc.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
-echo "export PATH=\$HOME/miniconda/bin:\$PATH" >> ~/.zshrc
-echo "export PATH=\$HOME/miniconda/bin:\$PATH" >> ~/.bashrc
-export PATH=$HOME/miniconda/bin:$PATH
+echo "export PATH=\$HOME/miniconda3/bin:\$PATH" >> ~/.zshrc
+echo "export PATH=\$HOME/miniconda3/bin:\$PATH" >> ~/.bashrc
+export PATH=$HOME/miniconda3/bin:$PATH
 conda create -n env3 python=3
 source activate env3
-conda install tensorflow-gpu=1.6 Pillow matlabplot
+conda install tensorflow-gpu=1.6 Pillow matplotlib ipython keras=2.1.5
 pip install opencv_python
